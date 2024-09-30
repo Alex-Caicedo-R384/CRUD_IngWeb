22aca550006f1f5067cbfba6a609e6f0f640c004
Alex_Caicedo_R384 <chevyagcr@gmail.com>
2024-09-30 02:20:53 -0500

    método store para poder guardar al usuario que se creo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,8 +32,21 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+    
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+    
+        return redirect()->route('users.index')->with('success', 'Usuario creado correctamente.');
     }
+    
 
     /**
      * Display the specified resource.
